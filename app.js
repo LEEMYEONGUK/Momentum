2c1120365f9f44b8e45afc9c9e786be00850da0e
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-06-19 23:07:59 +0900

    feat:login

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-// const loginForm = document.getElementById("login-form")
-// const loginInput = loginForm.querySelector("input")
-// const loginButton = loginForm.querySelector("button")
-
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
-// const loginButton = document.querySelector("#login-form button")
-
-// function onLoginBtnClick() {
-//   console.dir(loginInput)
-//   console.log("click")
-//   console.log("hello", loginInput.value)
-//   const username = loginInput.value
-  // if (username === "") {
-  //   alert("Please write your name")
-  // } else if (username.length > 15) {
-  //   alert("Your name is too long.")
-  // }
-// }
-
-// loginButton.addEventListener("click", onLoginBtnClick)
+const greeting = document.querySelector("#greeting")
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME)
   const username = loginInput.value
   console.log(username)
+  localStorage.setItem(USERNAME_KEY, username)
+  paintGreetings(username)
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
+function paintGreetings(username) {
+  // greeting.innerText = "Hello " + username
+  greeting.innerText = `Hello ${username}`
+  greeting.classList.remove(HIDDEN_CLASSNAME)
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+
+if(savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME)
+  loginForm.addEventListener("submit", onLoginSubmit)
+} else {
+  // show the greetings
+  paintGreetings(savedUsername)
+
+}
